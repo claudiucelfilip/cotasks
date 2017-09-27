08075d417992b9cb2cbbbf1f277eeac46f249dfa
Claudiu Teodor Filip <cfilip@opswat.com>
2017-09-27 19:48:54 +0300

    added absolute path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
   // ejs render automatically looks in the views folder
-  res.sendFile('index.html');
+  res.sendFile(path.resolve('index.html'));
 });
 
 app.listen(PORT, () => {
